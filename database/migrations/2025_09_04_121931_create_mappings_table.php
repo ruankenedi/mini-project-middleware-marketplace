17d8d7959f639668e84406eecc9bb3eefde33520
ruankenedi <ruankenedi01@gmail.com>
2025-09-05 14:55:57 -0300

    Marcações para orientar o código.

--- database/migrations/2025_09_04_121931_create_mappings_table.php
+++ database/migrations/2025_09_04_121931_create_mappings_table.php
@@ -11,10 +11,10 @@ class CreateMappingsTable extends Migration
         Schema::create('mappings', function (Blueprint $table) {
             $table->id();
             $table->string('mapping_type'); // 'department','category','store', etc.
-            $table->unsignedBigInteger('local_id'); // troca pontos local id (e.g. departments.id)
-            $table->foreignId('supplier_id')->constrained('suppliers');
-            $table->string('supplier_external_id')->nullable();
-            $table->string('supplier_external_name')->nullable();
+            $table->unsignedBigInteger('local_id'); // troca pontos local id (e.g. categories.id) 1
+            $table->foreignId('supplier_id')->constrained('suppliers'); // id categoria da magalu 3
+            $table->string('supplier_external_id')->nullable(); // id do fornecedor externo 1
+            $table->string('supplier_external_name')->nullable(); // magalu
             $table->timestamps();
 
             $table->index(['mapping_type', 'supplier_id', 'supplier_external_id']); // facilitates the search
